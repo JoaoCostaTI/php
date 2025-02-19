87071b32e5772d6fc014cd5be7fa926fea7e6c46
JoaoCostaTI <joao.costapp@outlook.com>
2025-02-19 14:07:04 -0300

    inicio Do Curso

--- ex001/index.php
+++ ex001/index.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Me livrando da Maldição</title>
+    <title>Dados do servidor</title>
 </head>
 <body>
-    <h1>
+    <h1>Dados do servidor</h1>
+
         <?php 
-            echo"Olá, Mundo! \u{1F30E}";
+            phpinfo();
         ?>
-    </h1>
-    <p>Vamos tentar nos livrar da Maldição</p>
 </body>
 </html>

42396e823a9a9536766a09aafa32bd07a39b0efc
JoaoCostaTI <joao.costapp@outlook.com>
2025-03-06 09:05:21 -0300

    Funcoes e Aritmeticas

---
+++ arena-sem-ex/expressoesAritmeticas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arena</title>
+</head>
+<body>
+    <?php 
+
+        // Ordem de precedencia
+        $resultado = 50 / (2 + 3) ** 2;
+
+        echo "O resultado é: $resultado";
+    ?>
+</body>
+</html>
---
+++ arena-sem-ex/funcoesAritmeticas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções</title>
+</head>
+<body>
+    <?php 
+        $r = sqrt(81);
+
+        echo"A resposta é: $r";
+    ?>
+</body>
+</html>
